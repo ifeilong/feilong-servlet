e7a9fd7b96e9d889d679e5dbca66e85435bf8eea
feilong <venusdrogon@163.com>
2016-03-27 05:02:00 +0800

    update javadoc

--- src/main/java/com/feilong/servlet/http/entity/HttpHeaders.java
+++ src/main/java/com/feilong/servlet/http/entity/HttpHeaders.java
@@ -291,6 +291,23 @@ public final class HttpHeaders{
      * 它是一个特殊字符串头，使得服务器能够识别客户使用的操作系统及版本、CPU 类型、浏览器及版本、浏览器渲染引擎、浏览器语言、浏览器插件等。
      * </p>
      * 
+     * <pre>
+    *  userAgent 属性是一个只读的字符串，声明了浏览器用于 HTTP 请求的用户代理头的值。
+    *  一般来讲，它是在 navigator.appCodeName 的值之后加上斜线和 navigator.appVersion 的值构成的。
+    *  例如：Mozilla/4.0 (compatible; MSIE6.0; Windows NT 5.2; SV1; .NET CLR 1.1.4322)。
+    *  注：用户代理头：user-agent header。
+    * 
+    *  User Agent中文名为用户代理，简称 UA，它是一个特殊字符串头，使得服务器能够识别客户使用的操作系统及版本、CPU 类型、浏览器及版本、浏览器渲染引擎、浏览器语言、浏览器插件等。
+    * 
+    *  标准格式为： 
+    *  浏览器标识 (操作系统标识; 加密等级标识; 浏览器语言) 渲染引擎标识 版本信息
+    *  
+    *  //Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.57 Safari/537.17
+    *  
+    *  iphone :Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_0 like Mac OS X; en-us) AppleWebKit/532.9 (KHTML, like Gecko) Version/4.0.5 Mobile/8A293 Safari/6531.22.7
+     * 
+     * </pre>
+     * 
      * @see <a href="http://tools.ietf.org/html/rfc2616#section-14.43">14.43 User-Agent</a>
      */
     public static final String USER_AGENT                  = "User-Agent";
